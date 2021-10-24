7ed23923e42ad05db13fab177cff18ee377eec1c
Wesley <wesley.brazil@outlook.com>
2021-10-24 13:23:17 -0300

    meu primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render(){
+    return(
+      <div>
+        <img src={require("./assets/biscoito")} alt='biscoito da sorte'/>
+        <Botao /> 
+      </div>
+    )
+  }
+}
+
+class Botao extends Component {
+  render(){
+    return(
+      <div>
+        <button>Abrir biscoito</button>
+      </div>
+    )
+  }
 }
 
 export default App;
